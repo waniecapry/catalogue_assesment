9335f2c38da4a561cb193f8c17ee163a50ffc02e
wanipaksya <norhazwanirazak9095@gmail.com>
2026-09-16 13:57:38 +0800

    feat: Add Assortment class with app name and description

---
+++ lib/assortment.dart
@@ -0,0 +1,5 @@
+class Assortment // This class represents the app. Assortment stands for Product Catalogue. Anything constants, variables, methods that belong to the app should be placed here
+{
+    static const String appName = "Assorta"; // constants with lowerCamelCase following Dart convention.
+    static const String appDescription = "Experience the finest way to shop";
+}

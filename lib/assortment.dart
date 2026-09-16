3a92da6b451c65821d859f32013d688595db5230
wanipaksya <norhazwanirazak9095@gmail.com>
2026-09-16 14:00:04 +0800

    feat: Implement product list and search methods in Assortment class

--- lib/assortment.dart
+++ lib/assortment.dart
@@ -2,4 +2,17 @@ class Assortment // This class represents the app. Assortment stands for Product
 {
     static const String appName = "Assorta"; // constants with lowerCamelCase following Dart convention.
     static const String appDescription = "Experience the finest way to shop";
+
+    static Future<List<Map<String, Object>>> getProductList( int offset, int limit ) async
+    {
+        final List<Map<String, Object>> products = [];
+
+        return products;
+    }
+    static Future<List<Map<String, Object>>> searchProduct( String query, int offset, int limit ) async
+    {
+        final List<Map<String, Object>> products = [];
+
+        return products;
+    }
 }

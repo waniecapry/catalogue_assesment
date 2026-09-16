94a50d2c2bf8a2b6bbdac73ceaaced1431ad0f7f
wanipaksya <norhazwanirazak9095@gmail.com>
2026-09-16 14:11:20 +0800

    feat: Update Assortment class to enhance product list and search functionality with improved data handling

--- lib/assortment.dart
+++ lib/assortment.dart
@@ -1,17 +1,46 @@
+import "dart:developer";
+
+import "package:dio/dio.dart"; // Using dio for handling the HTTP client.
+
 class Assortment // This class represents the app. Assortment stands for Product Catalogue. Anything constants, variables, methods that belong to the app should be placed here
 {
     static const String appName = "Assorta"; // constants with lowerCamelCase following Dart convention.
     static const String appDescription = "Experience the finest way to shop";
+    static final Dio _dio = Dio();
 
-    static Future<List<Map<String, Object>>> getProductList( int offset, int limit ) async
+    static Future<List<Map<String, dynamic>>> getProductList( int offset, int limit ) async
     {
-        final List<Map<String, Object>> products = [];
+        final List<Map<String, dynamic>> products = [];
+
+        Response r = await _dio.get
+        (
+            "https://dummyjson.com/products",
+            queryParameters: { "limit": limit, "skip": offset },
+            options: Options( responseType: ResponseType.json )
+        );                                                                          log( "Response data: ${r.data}" );
+
+        for( final product in r.data["products"] )
+        {
+            products.add( Map<String, dynamic>.from( product ) );
+        }                                                                           log( "products: $products" );
 
         return products;
     }
-    static Future<List<Map<String, Object>>> searchProduct( String query, int offset, int limit ) async
+    static Future<List<Map<String, dynamic>>> searchProduct( String query, int offset, int limit ) async
     {
-        final List<Map<String, Object>> products = [];
+        final List<Map<String, dynamic>> products = [];
+
+        Response r = await _dio.get
+        (
+            "https://dummyjson.com/products/search",
+            queryParameters: { "q": query, "limit": limit, "skip": offset },
+            options: Options( responseType: ResponseType.json ),
+        );                                                                          log( "Response data: ${r.data}" );
+
+        for( final product in r.data["products"] )
+        {
+            products.add( Map<String, dynamic>.from( product ) );
+        }                                                                           log( "products: $products" );
 
         return products;
     }
